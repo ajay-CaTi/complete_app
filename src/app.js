9ce2064acd731bfeeb41062a7704ff1c6d442c71
ajay-CaTi <ajaykumarsingh110@gmail.com>
2025-03-11 13:53:15 +0530

    added apierror, apiresponse ,asynchandler: to handle asynchronous request+trycatch

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+module.exports = { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 4400;
-const { DB_NAME } = require("./constants");
 const { connectDB } = require("./db");
-const { aa } = require("../mm.js");
-console.log(aa);
-console.log(process.env.MONGODB_URL, DB_NAME, PORT);
-connectDB();
+const { app } = require("./app.js");
+console.log(PORT);
+
+// DB function call
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listen on port: ${PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("our app is not able to talk to db", error);
+      throw error;
+    }); // listners listen on many events | may be our apps wont able to talk
+  })
+  .catch((error) => {
+    console.log("Error occured", error);
+  });
 
 // (async () => {
 //   try {
